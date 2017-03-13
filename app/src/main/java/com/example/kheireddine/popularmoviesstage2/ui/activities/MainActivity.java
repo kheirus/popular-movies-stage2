f166545279455edb48224a3afa9d425a2ec1f7fa
kheireddine <kheirus@hotmail.com>
2017-03-13 16:01:24 +0100

    simple BrodcastReceiver that checks when device is charging

--- app/src/main/java/com/example/kheireddine/popularmoviesstage2/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/kheireddine/popularmoviesstage2/ui/activities/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.kheireddine.popularmoviesstage2.ui.activities;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.kheireddine.popularmoviesstage2.R;
 import com.example.kheireddine.popularmoviesstage2.ui.fragments.MainFragment;
+import com.example.kheireddine.popularmoviesstage2.utils.Utils;
 
 public class MainActivity extends AppCompatActivity {
-
+    IntentFilter mChargingIntentFilter;
+    ChargingBroadCastReceiver mChargingBroadcastReceiver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,8 +26,24 @@ public class MainActivity extends AppCompatActivity {
             addGridFragment();
         }
 
+        mChargingIntentFilter = new IntentFilter();
+        mChargingIntentFilter.addAction(Intent.ACTION_POWER_CONNECTED);
+        mChargingIntentFilter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+
+        mChargingBroadcastReceiver = new ChargingBroadCastReceiver();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(mChargingBroadcastReceiver, mChargingIntentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mChargingBroadcastReceiver);
+    }
 
     public void addGridFragment() {
         if (!isFinishing()) {
@@ -34,7 +56,27 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    // Test the Broadcast Receiver
+    private void showCharging(boolean isCharging){
+        if (isCharging){
+            Utils.showLongToastMessage(this, "Message from BroadcastReceiver : The device is charging");
+        } else {
+            Utils.showLongToastMessage(this, "Message from BroadcastReceiver : The device is NOT charging");
+        }
+    }
 
+    /**
+     * BroadcastReceiver class that checks if the device is charging or not
+     * */
+    private class ChargingBroadCastReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action =  intent.getAction();
+            boolean isCharging = action.equals(Intent.ACTION_POWER_CONNECTED);
+            showCharging(isCharging);
+        }
+    }
 
 }
 
